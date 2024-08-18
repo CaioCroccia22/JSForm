0f19cb67e03e591d9ddfe1914dbe4d1b62583c64
caiocroccia22 <caiocroccia22@gmail.com>
2024-08-17 22:51:44 -0300

    Fix: preventDefault and compare numeric values correctly

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-console.log('teste');
 const buttonSubmit = document.querySelector('.btn-submit');
 const inputNumberA = document.querySelector('#number_A');
+const inputNumberB = document.querySelector('#number_B');
 
 buttonSubmit.addEventListener('click', function(e){
+    e.preventDefault();
     console.log('Apertando botão'); 
-    if (inputNumberA.value === ''){
-        console.log('Esta vazio o numero A');
-        alert('Esta vazio o numero A')
-        inputNumberA.style.border = 'solid red 1px';
+
+    if (parseFloat(inputNumberA.value) >= parseFloat(inputNumberB.value)){
+        alert('O número A dever ser menor que o número B');
+    }
+    else{
+        alert('Os números estão corretos')
     }
 });
